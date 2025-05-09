376e09b9cd99d5cd1fa4fe6a6fbcb543ac9b3059
kukuhks <kukuhh.ks@gmail.com>
2025-05-09 17:06:06 +0700

    First commit

---
+++ Dadu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+
+	int dice_num = rand() % 6 + 1;
+	int guess_num;
+
+	cout << "main dadu\n";
+	cout << "Tebak angka: ";
+	cin >> guess_num;
+
+	if (guess_num == dice_num)
+		cout << "Tebakan benar\n";
+	else
+		cout << "Tebakan salah\n";
+
+	system("pause");
+	return 0;
+}
